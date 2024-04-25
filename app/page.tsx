0903d9f4cadde32c4b8287570195621c8fab4c41
atakanbaskor <atakanbaskor@hotmail.com>
2024-04-26 00:05:20 +0300

    Css

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Home() {
+  const allParagrafCss = {
+    backgroundColor: "#f2f2f2",
+    padding: "2rem",
+  };
   return (
     <React.Fragment>
       <div>
-        <p className="text-center mt-3">Hello World!!!</p>
+        <p className="text-center mt-3" style={allParagrafCss}>
+          Hello World!!!
+        </p>
+        <hr />
       </div>
     </React.Fragment>
   );
